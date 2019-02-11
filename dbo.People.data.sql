2d412fd9e68874372bbd60a54e602175487bc976
USER <USER@PONGO>
2019-02-11 17:29:09 -0600

    Add project files.

---
+++ dbo.People.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[People] ON
+INSERT INTO [dbo].[People] ([ContactId], [Name], [ Company], [Telephone], [Email], [Client], [LastCall]) VALUES (1, N'Lloyd Jones', N'Black Box ind', N'(718)555-5638', N'LJones@xbalackboxinc.com', 1, N'2010-05-26 00:00:00')
+INSERT INTO [dbo].[People] ([ContactId], [Name], [ Company], [Telephone], [Email], [Client], [LastCall]) VALUES (2, N'lucinda ericson', N'Ericson Events', N'(212)555-9523', N'Lucy@ericson.com', 0, N'2010-05-17 00:00:00')
+INSERT INTO [dbo].[People] ([ContactId], [Name], [ Company], [Telephone], [Email], [Client], [LastCall]) VALUES (3, N'liz nelson', N'JTP', N'(419)555-2578', N'liznelson@JTP.ORG', 1, N'2009-03-04 00:00:00')
+INSERT INTO [dbo].[People] ([ContactId], [Name], [ Company], [Telephone], [Email], [Client], [LastCall]) VALUES (4, N'Matt Franks', N'XYZ Industries', N'(212)555-8125', N'mat.franks@xyzindustries.com', 1, N'2010-05-26 00:00:00')
+INSERT INTO [dbo].[People] ([ContactId], [Name], [ Company], [Telephone], [Email], [Client], [LastCall]) VALUES (5, N'Sarah kalter', N'kalter, riddle and stoft', N'(614)555-5641', N'sarah@KRS.org', 0, NULL)
+SET IDENTITY_INSERT [dbo].[People] OFF
